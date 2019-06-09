c52db624e8744b1cd0c0f2f54a1ee39602b3dc61
PorryChen <porrychen@gmail.com>
2019-06-08 23:34:58 -0500

    add css, fonts, and js folds

---
+++ js/menu.js
@@ -0,0 +1,18 @@
+(function()
+{
+    function get_menu_data() {
+
+
+        return "{" + +  "}";
+    }
+
+    function display_menu(activeName)
+    {
+        var menu_data = get_menu_data();
+
+
+
+        var body = document.body;
+        body.classList.add("loaded");
+    }
+})();
