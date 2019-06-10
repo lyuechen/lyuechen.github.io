37820293daf4c4522115ed36e27b39b30bdae3e8
lyuechen <51253560+lyuechen@users.noreply.github.com>
2019-06-09 23:28:04 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -79,8 +79,9 @@ function display_weather() {
 var statusPos = 0;
 var welcomeMessage = "Welcome to My Website!"//"欢迎来到 我的小小世界！";
 function display_status_message() {
-    window.status = welcomeMessage.substring(statusPos, welcomeMessage.length) + "                   ";
-    welcomeMessage.substring(0, statusPos);
+    window.status = welcomeMessage.substring(statusPos, welcomeMessage.length) 
+        + "                   " 
+        + welcomeMessage.substring(0, statusPos);
     statusPos++;
 
     if (statusPos > welcomeMessage.length) statusPos = 0;
